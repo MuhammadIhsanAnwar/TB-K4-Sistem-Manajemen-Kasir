1b54b20ed665650d37630517e5eac482bc1f24e1
gabriel.christian1801 <gabriel.christian1801@gmail.com>
2024-10-03 15:00:53 +0700

    Penyesuaian Dataset

--- static/js/script.js
+++ static/js/script.js
@@ -5,105 +5,208 @@ setTimeout(() => {
         alert.classList.add('fade-out');
         setTimeout(() => {
             alert.remove();
-        }, 500); 
+        }, 500);
     });
 }, 3000);
 
 // Ambil data dari elemen JSON
-const barData = JSON.parse(document.getElementById('barData').textContent);
-const lineData = JSON.parse(document.getElementById('lineData').textContent);
-const pieData = JSON.parse(document.getElementById('pieData').textContent);  
-const areaData = JSON.parse(document.getElementById('areaData').textContent);  
-const provinsi = JSON.parse(document.getElementById('provinsi').textContent);
- 
-Highcharts.chart('barChart', {
+const pks_name = JSON.parse(document.getElementById('pks_name').textContent);
+const norma_ap = JSON.parse(document.getElementById('norma_ap').textContent);
+const losis_ap = JSON.parse(document.getElementById('losis_ap').textContent);
+const norma_bj = JSON.parse(document.getElementById('norma_bj').textContent);
+const losis_bj = JSON.parse(document.getElementById('losis_bj').textContent);
+const norma_tk = JSON.parse(document.getElementById('norma_tk').textContent);
+const losis_tk = JSON.parse(document.getElementById('losis_tk').textContent);
+const norma_da = JSON.parse(document.getElementById('norma_da').textContent);
+const losis_da = JSON.parse(document.getElementById('losis_da').textContent);
+const norma_sd = JSON.parse(document.getElementById('norma_sd').textContent);
+const losis_sd = JSON.parse(document.getElementById('losis_sd').textContent);
+const norma_pks = JSON.parse(document.getElementById('norma_pks').textContent);
+const losis_pks = JSON.parse(document.getElementById('losis_pks').textContent);
+
+Highcharts.chart('apChart', {
     chart: {
-        type: 'column'  
+        type: 'column'
     },
     title: {
-        text: 'Produksi per Provinsi'  
+        text: 'Losses Ampas Press'
     },
     xAxis: {
-        categories: provinsi  
+        categories: pks_name
     },
     yAxis: {
         min: 0,
         title: {
-            text: 'Produksi'  
+            text: 'Persentase (%)'
         }
     },
     series: [{
-        name: 'Produksi', 
-        data: barData, 
-        color: 'rgba(54, 162, 235, 0.2)', 
-        borderColor: 'rgba(54, 162, 235, 1)', 
-        borderWidth: 1  
-    }]
-}); 
-
-Highcharts.chart('lineChart', {
-    title: {
-        text: 'Luas Panen per Provinsi' 
-    },
-    xAxis: {
-        categories: provinsi 
-    },
-    yAxis: {
-        title: {
-            text: 'Luas Panen'    
-        }
-    },
-    series: [{
-        name: 'Luas Panen', 
-        data: lineData,  
-        type: 'line',  
-        fill: false,
-        color: 'rgba(255, 99, 132, 1)',  
-        lineWidth: 2  
-    }]
-});
- 
-Highcharts.chart('pieChart', {
-    chart: {
-        type: 'pie'  
-    },
-    title: {
-        text: 'Distribusi Produksi per Provinsi'  
-    },
-    series: [{
-        name: 'Produksi',
-        data: pieData,  
-        colorByPoint: true  
+        name: 'Norma Ampas Press',
+        data: norma_ap,
+        borderWidth: 1
+    }, {
+        name: 'Losses % Ampas Press',
+        data: losis_ap,
+        borderWidth: 1
     }]
 });
 
-Highcharts.chart('areaChart', {
+Highcharts.chart('bjChart', {
     chart: {
-        type: 'area'  
+        type: 'column'
     },
     title: {
-        text: 'Curah Hujan per Provinsi'  
+        text: 'Losses Biji'
     },
     xAxis: {
-        categories: provinsi  
+        categories: pks_name
     },
     yAxis: {
+        min: 0,
         title: {
-            text: 'Curah Hujan'  
+            text: 'Persentase (%)'
         }
     },
     series: [{
-        name: 'Curah Hujan',  
-        data: areaData, 
+        name: 'Norma Biji',
+        data: norma_bj,
+        borderWidth: 1
+    }, {
+        name: 'Losses % Biji',
+        data: losis_bj,
+        borderWidth: 1
+    }]
+});
+
+Highcharts.chart('tkChart', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Losses Tandan Kosong'
+    },
+    xAxis: {
+        categories: pks_name
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Persentase (%)'
+        }
+    },
+    series: [{
+        name: 'Norma Tandan Kosong',
+        data: norma_tk,
+        borderWidth: 1
+    }, {
+        name: 'Losses % Tandan Kosong',
+        data: losis_tk,
+        borderWidth: 1
+    }]
+});
+
+Highcharts.chart('sdChart', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Losses Solid Decanter'
+    },
+    xAxis: {
+        categories: pks_name
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Persentase (%)'
+        }
+    },
+    series: [{
+        name: 'Norma Solid Decanter',
+        data: norma_sd,
+        borderWidth: 1
+    }, {
+        name: 'Losses % Solid Decanter',
+        data: losis_sd,
+        borderWidth: 1
+    }]
+});
+
+Highcharts.chart('daChart', {
+    chart: {
+        type: 'area'
+    },
+    title: {
+        text: 'Losses Drab Akhir'
+    },
+    xAxis: {
+        categories: pks_name
+    },
+    yAxis: {
+        title: {
+            text: 'Persentase (%)'
+        }
+    },
+    series: [{
+        name: 'Norma Drab Akhir',
+        data: norma_da,
+        marker: {
+            enabled: false
+        }
+    }, {
+        name: 'Losses % Drab Akhir',
+        data: losis_da,
         fillColor: {
-            linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 }, 
+            linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
             stops: [
-                [0, 'rgba(255, 99, 132, 1)'],  
-                [1, 'rgba(255, 99, 132, 0.1)']  
+                [0, 'rgba(255, 99, 132, 1)'],
+                [1, 'rgba(255, 99, 132, 0.3)']
             ]
         },
         marker: {
-            enabled: false  
+            enabled: false
         }
     }]
+});
+
+Highcharts.chart('pksChart', {
+    chart: {
+        type: 'line'
+    },
+    title: {
+        text: 'Losses PKS',
+    },
+    xAxis: {
+        categories: pks_name
+    },
+    yAxis: {
+        title: {
+            text: 'Persentase (%)'
+        }
+    },
+    tooltip: {
+        shared: true,
+        crosshairs: true
+    },
+    series: [{
+        name: 'Norma PKS',
+        data: norma_pks
+    }, {
+        name: 'Losses % PKS',
+        data: losis_pks
+    }],
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    align: 'center',
+                    verticalAlign: 'bottom',
+                    layout: 'horizontal'
+                }
+            }
+        }]
+    }
 });
