90d86517fc610506a473768fe8562bd1d31a955a
Muhammad Ihsan Anwar <muhammadihsananwar3@gmail.com>
2025-12-05 23:18:08 +0700

    Update

--- File Utama.c
+++ File Utama.c
@@ -1,16 +1,103 @@
+// Tugas Besar Kelompok 4 - Sistem Manajemen Kasir
+// 1. Muhammad Ihsan Anwar - 251402044 
+// 2. Nadine Tantiara Hutagaol - 251402050
+// 3. Rizky Cristian Fero Sihombing - 251402056
+// 4. Juda Benhur Turnip - 251402096
+// 5. Salsabila Salwa Rizki - 251402123
+
+// File yang diinput: produk.txt
+// Format file produk.txt:
+// nomor|nama|harga|stok
+
+// File yang dioutput: kwitansi_1.txt, kwitansi_2.txt, ..., laporan_transaksi_1.txt, laporan_transaksi_2.txt, ...
+
 #include <stdio.h>
+#include <time.h>
+#include <dirent.h>
+#include <string.h>
 
-int main()
+// ===== STRUKTUR DATA =====
+struct Product
 {
-    int a;
-    int b;
-    int c;
-    printf("tes lagi");
-scanf("%d", a);
+    int id;
+    char nama[50];
+    int harga;
+    int stok;
+};
 
-printf("tes lagi 2");
-scanf("%d", b);
+struct ItemTransaksi
+{
+    int idProduk;
+    int qty;
+    int subtotal;
+};
 
-return 0;
+struct Product products[100];
+int jumlahProduk = 0;
 
+// ====== KERANJANG GLOBAL - TIDAK HILANG WALAU KELUAR KE MENU UTAMA ======
+struct ItemTransaksi keranjang[50];
+int jumlahItem = 0;
+
+// ===== INPUT FILE PRODUK =====
+void loadProduk()
+{
+    FILE *f = fopen("produk.txt", "r");
+    if (!f)
+        return;
+
+    while (!feof(f))
+    {
+        fscanf(f, "%d|%49[^|]|%d|%d\n",
+               &products[jumlahProduk].id,
+               products[jumlahProduk].nama,
+               &products[jumlahProduk].harga,
+               &products[jumlahProduk].stok);
+        jumlahProduk++;
+    }
+    fclose(f);
+}
+
+void simpanProduk()
+{
+    FILE *f = fopen("produk.txt", "w");
+    for (int i = 0; i < jumlahProduk; i++)
+    {
+        fprintf(f, "%d|%s|%d|%d\n",
+                products[i].id,
+                products[i].nama,
+                products[i].harga,
+                products[i].stok);
+    }
+    fclose(f);
+}
+
+// ===== MENCARI PRODUK =====
+int cariProdukById(int id)
+{
+    for (int i = 0; i < jumlahProduk; i++)
+    {
+        if (products[i].id == id)
+            return i;
+    }
+    return -1;
+}
+
+// ===== MENAMPILKAN PRODUK =====
+void tampilkanProduk()
+{
+    printf("\n===== DAFTAR PRODUK =====\n");
+    printf("%-5s %-20s %-10s %-10s\n", "ID", "Nama", "Harga", "Stok");
+    printf("-------------------------------------------------------\n");
+
+    for (int i = 0; i < jumlahProduk; i++)
+    {
+        printf("%-5d %-20s Rp %-10d %-10d\n",
+               products[i].id,
+               products[i].nama,
+               products[i].harga,
+               products[i].stok);
+    }
+
+    printf("-------------------------------------------------------\n");
 }
